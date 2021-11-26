69d86756adbb8a54e284043e2d689023bafaac7b
aashiqAhamedSP <67874080+aashiqAhamedSP@users.noreply.github.com>
2021-11-26 19:11:01 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var expressapp = express()
 expressapp.get('/', function (req, res) {
     res.send('{"message":"Hello World Javascript Nuvepro v1 from Azure pipeline now"}')
 })
-expressapp.listen(5000, function () {
-    console.log('Ready on port 5000!')
+expressapp.listen(5001, function () {
+    console.log('Ready on port 5001!')
 })
 
 /* To Handle Ctrl C*/

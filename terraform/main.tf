c520c5ee7a89056ac9076fa24ed2cfb5f7ffd78c
Sharukh Ahamed <Aashiq220596@outlook.com>
2021-11-30 19:47:19 +0000

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,27 +3,36 @@ provider "azurerm" {
 
 }
 
-resource "azurerm_resource_group" "project2_rg" {
-  name     = "__resourcegroupname__"
-  location = "__location__"
+terraform {
+    backend "azurerm" {
+        resource_group_name = "terrarg"
+        storage_account_name = "terraformstorageaccoun"
+        container_name = "terraform"
+        key = "terraform.tfstate"
+    }
+}
+
+resource "azurerm_resource_group" "lab3kuberg" {
+  name     = "lab3kuberg"
+  location = "southindia"
 }
 
 data "azurerm_container_registry" "acr" {
-  name                = "__acrname__"
-  resource_group_name = "test"
+  name                = "kubenodejsacr"
+  resource_group_name = "acrrg"
 }
 
 
-resource "azurerm_kubernetes_cluster" "wolkweb_aks" {
-  name                = "__akscluster__"
-  location            = azurerm_resource_group.project2_rg.location
-  resource_group_name = azurerm_resource_group.project2_rg.name
-  dns_prefix          = "wolkwebaks"
+resource "azurerm_kubernetes_cluster" "lab3_aks" {
+  name                = "lab3_aks"
+  location            = azurerm_resource_group.lab3kuberg.location
+  resource_group_name = azurerm_resource_group.lab3kuberg.name
+  dns_prefix          = "lab3_aks"
 
   default_node_pool {
-    name       = "wolknodepl"
-    node_count = 1
-    vm_size    = "Standard_DS2_v2"
+    name       = "lab3aksnode"
+    node_count = 4
+    vm_size    = "Standard_B2ms"
     enable_auto_scaling = false
   }
 
@@ -33,8 +42,8 @@ resource "azurerm_kubernetes_cluster" "wolkweb_aks" {
 }
 
 data "azurerm_user_assigned_identity" "aksmi" {
-  name                = "${azurerm_kubernetes_cluster.wolkweb_aks.name}-agentpool"
-  resource_group_name = "MC_${azurerm_resource_group.project2_rg.name}_${azurerm_kubernetes_cluster.wolkweb_aks.name}_${azurerm_resource_group.project2_rg.location}"
+  name                = "${azurerm_kubernetes_cluster.lab3_aks.name}-agentpool"
+  resource_group_name = "MC_${azurerm_resource_group.lab3kuberg.name}_${azurerm_kubernetes_cluster.lab3_aks.name}_${azurerm_resource_group.lab3kuberg.location}"
 }
 
 resource "azurerm_role_assignment" "role_Contributor" {

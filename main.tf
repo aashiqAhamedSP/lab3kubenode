022801e006f6637f85e5977c3d77509198c82c82
aashiqAhamedSP <67874080+aashiqAhamedSP@users.noreply.github.com>
2021-12-01 22:19:14 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,3 @@
-variable "subid" {
-    type = string
-}
-
-variable "clid" {
-    type = string
-  
-}
-
-variable "tnid" {
-  type = string
-}
-
-variable "clcr" {
-  type = string
-}
-
-variable "ansssh" {
-  type = string
-}
-
 provider "azurerm" {
   features {}
   subscription_id = var.subid

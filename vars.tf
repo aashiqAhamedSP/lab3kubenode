e63afe4dec93a997d106b9f7f4655a301f65d07e
aashiqAhamedSP <67874080+aashiqAhamedSP@users.noreply.github.com>
2021-12-01 22:19:40 +0000

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,20 @@
+variable "subid" {
+    type = string
+}
+
+variable "clid" {
+    type = string
+  
+}
+
+variable "tnid" {
+  type = string
+}
+
+variable "clcr" {
+  type = string
+}
+
+variable "ansssh" {
+  type = string
+}
